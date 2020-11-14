d9f56662133732a32e7dea7e90d4c393c95f96fd
朱彦琛 <2652162320@qq.com>
2020-11-14 22:54:35 +0800

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,31 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+# 1.导入系统的logging
+import logging
+# 2.创建（获取）日志器
+logger = logging.getLogger('django')
+def log(request):
+    # 3. 使用日志器记录信息
+    logger.info('info')
+    return HttpResponse('test')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', log),
+]
